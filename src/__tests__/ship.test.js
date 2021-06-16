707657311aca04dd99039a0c07afaafe31b67909
Riley Loudon <rileyloudon@icloud.com>
2021-06-15 22:22:19 -0500

    add null to shipFactory for position

--- src/__tests__/ship.test.js
+++ src/__tests__/ship.test.js
@@ -1,14 +1,14 @@
 import shipFactory from '../factories/ship';
 
 test('Ship registers hits', () => {
-  const testShip = shipFactory(3);
+  const testShip = shipFactory(null, null, 3);
   testShip.hit(0);
   testShip.hit(3);
   expect(testShip.hitSpots).toStrictEqual([true, false, false, true]);
 });
 
 test('Ship sinks', () => {
-  const sinkShip = shipFactory(1);
+  const sinkShip = shipFactory(null, null, 2);
   sinkShip.hit(0);
   sinkShip.hit(1);
   expect(sinkShip.isSunk()).toBe(true);
