2063fce5f6f801ecf2d690f6df6963f574819985
Riley Loudon <rileyloudon@icloud.com>
2021-06-19 19:49:11 -0500

    test computer is able to attack

---
+++ __tests__/player.test.js
@@ -0,0 +1,28 @@
+import playerFactory from '../src/factories/player';
+import gameboardFactory from '../src/factories/gameboard';
+import shipFactory from '../src/factories/ship';
+import { afterEach, expect, jest } from '@jest/globals';
+
+// Testing random returns with jest:
+// https://stackoverflow.com/a/57730344/12843016
+
+beforeEach(() => jest.spyOn(global.Math, 'random').mockReturnValue(0.45));
+
+afterEach(() => jest.spyOn(global.Math, 'random').mockRestore());
+
+test('Computer picks a random target', () => {
+  const testGameboard = gameboardFactory();
+  const testPlayer = playerFactory('computer', testGameboard);
+  testGameboard.placeShip(4, 2, 5, 'horizontal');
+
+  testPlayer.attack();
+  testPlayer.attack();
+
+  expect(testGameboard.map.row[4][4].hitSpots).toEqual([
+    false,
+    false,
+    true,
+    true,
+    false,
+  ]);
+});
